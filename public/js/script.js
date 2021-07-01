64ab1fd8b10d5b57710a90330eecb622006c874d
Glitch (exposysdata) <none>
2021-07-01 07:52:09 +0000

    🎱🎃 Checkpoint ./public/js/script.js:8039762/914

--- public/js/script.js
+++ public/js/script.js
@@ -578,7 +578,7 @@ $.ajaxSetup({
 
 if ('serviceWorker' in navigator) {
 	  window.addEventListener('load', function() {
-	    navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+	    navigator.serviceWorker.register('sw.js').then(function(registration) {
 	      // Registration was successful
 	      console.log('ServiceWorker registration successful with scope: ', registration.scope);
 	    }, function(err) {
@@ -587,32 +587,3 @@ if ('serviceWorker' in navigator) {
 	    });
 	  });
 	}
-
-	let deferredPrompt;
-	var div = document.querySelector('.add-to');
-	var button = document.querySelector('.add-to-btn');
-
-	window.addEventListener('beforeinstallprompt', (e) => {
-	  // Prevent Chrome 67 and earlier from automatically showing the prompt
-	  e.preventDefault();
-	  // Stash the event so it can be triggered later.
-	  deferredPrompt = e;
-	  div.style.display = 'block';
-
-	  button.addEventListener('click', (e) => {
-	  // hide our user interface that shows our A2HS button
-	  div.style.display = 'none';
-	  // Show the prompt
-	  deferredPrompt.prompt();
-	  // Wait for the user to respond to the prompt
-	  deferredPrompt.userChoice
-	    .then((choiceResult) => {
-	      if (choiceResult.outcome === 'accepted') {
-	        console.log('User accepted the A2HS prompt');
-	      } else {
-	        console.log('User dismissed the A2HS prompt');
-	      }
-	      deferredPrompt = null;
-	    });
-	});
-	});
